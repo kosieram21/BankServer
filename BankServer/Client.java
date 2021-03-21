be5ee7b222032aeb5c885d990936f1247abff0b4
Mitch Kosieradzki <mitchkos21@gmail.com>
2021-03-21 03:29:46 -0500

    simpler client

--- BankServer/Client.java
+++ BankServer/Client.java
@@ -1,9 +1,6 @@
 package BankServer;
 
-import java.io.IOException;
-import java.rmi.NotBoundException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
